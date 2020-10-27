764a2b5c7e0571597b3f01945492baab3fcc4a15
unknown <59000491+sinarmedcon@users.noreply.github.com>
2020-10-27 15:53:12 +0700

    sihpedia

---
+++ app/Views/back/home.php
@@ -0,0 +1,11 @@
+<?= $this->extend('back/layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+
+<main class="page landing-page">
+    <h1>ini ADMIN</h1>
+
+</main>
+
+<?= $this->endSection(); ?>
